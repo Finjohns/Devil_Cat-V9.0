cd0bf8d70a24734fd1c087ab3f7797164959b733
Finjohns <kelvinonginjo08@gmail.com>
2024-08-17 20:12:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 //Instagram: techgod143
 //Telegram: t.me/techgod143
 //GitHub: @techgod143
-//WhatsApp: +917466008456
+//WhatsApp: +254784320958
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@techgod143
 
 require('./settings')
